3892d447473f5ee061aed40a595da82593aaaeaf
youfoundmalik <godswillugochukwu88@gmail.com>
2021-10-10 19:22:55 +0100

    calroies percentage tracker and code format

--- src/components/BasketFruit.js
+++ src/components/BasketFruit.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import './Fruit.scss'
+import React from "react";
+import "./Fruit.scss";
 
-const BasketFruit = ({image,alt,left,top}) => {
-    return (
-        <div className= "fruit-in-basket" style={{left: `${left}`, top: `${top}`}}>
-            <img src={image} alt={alt}/>
-        </div>
-    )
-}
+const BasketFruit = ({ image, alt, left, top }) => {
+  return (
+    <div className="fruit-in-basket" style={{ left: `${left}`, top: `${top}` }}>
+      <img src={image} alt={alt} />
+    </div>
+  );
+};
 
-export default BasketFruit
+export default BasketFruit;
--- src/pages/LevelOne.js
+++ src/pages/LevelOne.js
@@ -1,147 +1,192 @@
-import React, { useState } from 'react'
-import { Helmet } from 'react-helmet'
-import Header from '../components/Header'
-import './LevelOne.scss'
+import React, { useState } from "react";
+import { Helmet } from "react-helmet";
+import Header from "../components/Header";
+import "./LevelOne.scss";
 
-import stall from '../images/Lvl1/Yoda_Stall.png'
-import basket from '../images/Lvl1/Basket.png'
-import Fruit from '../components/Fruit'
+import stall from "../images/Lvl1/Yoda_Stall.png";
+import basket from "../images/Lvl1/Basket.png";
+import Fruit from "../components/Fruit";
 
-import Banana from '../images/Lvl1/Yoda_Fruit-Banana.png'
-import Blueberry from '../images/Lvl1/Yoda_Fruit-Blueberry.png'
-import Cherry from '../images/Lvl1/Yoda_Fruit-Cherry.png'
-import Coconut from '../images/Lvl1/Yoda_Fruit-Coconut.png'
-import Pineapple from '../images/Lvl1/Yoda_Fruit-Pineapple.png'
-import Watermelon from '../images/Lvl1/Yoda_Fruit-Watermelon.png'
-import Footer from '../components/Footer'
-import { useDrop } from 'react-dnd'
-import BasketFruit from '../components/BasketFruit'
+import Banana from "../images/Lvl1/Yoda_Fruit-Banana.png";
+import Blueberry from "../images/Lvl1/Yoda_Fruit-Blueberry.png";
+import Cherry from "../images/Lvl1/Yoda_Fruit-Cherry.png";
+import Coconut from "../images/Lvl1/Yoda_Fruit-Coconut.png";
+import Pineapple from "../images/Lvl1/Yoda_Fruit-Pineapple.png";
+import Watermelon from "../images/Lvl1/Yoda_Fruit-Watermelon.png";
+import Footer from "../components/Footer";
+import { useDrop } from "react-dnd";
+import BasketFruit from "../components/BasketFruit";
 
-import guage0 from '../images/Yoda_Gauge-0.png'
-import guage25 from '../images/Yoda_Gauge-25.png'
-import guage50 from '../images/Yoda_Gauge-50.png'
-import guage100 from '../images/Yoda_Gauge-100.png'
-
-
+import guage0 from "../images/Yoda_Gauge-0.png";
+import guage25 from "../images/Yoda_Gauge-25.png";
+import guage50 from "../images/Yoda_Gauge-50.png";
+import guage75 from "../images/Yoda_Gauge-75.png";
+import guage100 from "../images/Yoda_Gauge-100.png";
 
 const fruits = [
-    {id: '1', fruit: 'Banana', image: `${Banana}`, cal: '7', price: '7', left: '10%', top: '25%'},
-    {id: '2', fruit: 'Blueberry', image: `${Blueberry}`, cal: '5', price: '2', left: '30%', top: '15%'},
-    {id: '3', fruit: 'Cherry', image: `${Cherry}`, cal: '2', price: '3', left: '25%', top: '55%'},
-    {id: '4', fruit: 'Coconut', image: `${Coconut}`, cal: '8', price: '10', left: '35%', top: '55%'},
-    {id: '5', fruit: 'Pineapple', image: `${Pineapple}`, cal: '12', price: '5', left: '60%', top: '55%'},
-    {id: '6', fruit: 'Watermelon', image: `${Watermelon}`, cal: '4', price: '8', left: '50%', top: '55%'}
-]
+  {
+    id: "1",
+    fruit: "Banana",
+    image: `${Banana}`,
+    cal: "7",
+    price: "7",
+    left: "10%",
+    top: "25%",
+  },
+  {
+    id: "2",
+    fruit: "Blueberry",
+    image: `${Blueberry}`,
+    cal: "5",
+    price: "2",
+    left: "30%",
+    top: "15%",
+  },
+  {
+    id: "3",
+    fruit: "Cherry",
+    image: `${Cherry}`,
+    cal: "2",
+    price: "3",
+    left: "25%",
+    top: "55%",
+  },
+  {
+    id: "4",
+    fruit: "Coconut",
+    image: `${Coconut}`,
+    cal: "8",
+    price: "10",
+    left: "35%",
+    top: "55%",
+  },
+  {
+    id: "5",
+    fruit: "Pineapple",
+    image: `${Pineapple}`,
+    cal: "12",
+    price: "5",
+    left: "60%",
+    top: "55%",
+  },
+  {
+    id: "6",
+    fruit: "Watermelon",
+    image: `${Watermelon}`,
+    cal: "4",
+    price: "8",
+    left: "50%",
+    top: "55%",
+  },
+];
 
 const LevelOne = () => {
+  const [fruitList, setFruitsList] = useState(fruits);
 
-    const [fruitList, setFruitsList] = useState(fruits)
+  const [inBasket, setInBasket] = useState([]);
+  const [counter, setCounter] = useState(0);
+  const [cash, setCash] = useState(25);
+  const [calories, setCalories] = useState(0);
 
-    const [inBasket, setInBasket] = useState([]);
-    const [counter, setCounter] = useState(0);
-    const [cash, setCash] = useState(25);
-    const [calories, setCalories] = useState(0)
+  const [{ isOver }, drop] = useDrop(() => ({
+    accept: "card",
+    drop: (item) => addFruitToBasket(item.id),
+    collect: (monitor) => ({
+      isOver: !!monitor.isOver(),
+    }),
+  }));
 
-    const [{isOver}, drop] = useDrop(()=>({
-        accept: "card",
-        drop: (item) => addFruitToBasket(item.id),
-        collect: (monitor) => ({
-            isOver: !!monitor.isOver()
-        })
-    }))
+  const addFruitToBasket = (id) => {
+    var draggedFruit = fruitList.filter((fruit) => id === fruit.id);
 
-    const addFruitToBasket = (id) => {
+    setInBasket((inBasket) => {
+      return [...inBasket, { ...draggedFruit[0] }];
+    });
 
-        const draggedFruit = fruitList.filter((fruit) => id === fruit.id)
+    setFruitsList((fruitList) => {
+      return fruitList.filter((fruit) => id !== fruit.id);
+    });
 
-        setInBasket((inBasket)=>{
-            return [...inBasket, {...draggedFruit[0]}]
-        })
+    setCash((cash) => {
+      return parseInt(cash) - parseInt(draggedFruit[0].price);
+    });
 
-        setFruitsList((fruitList)=>{
-            return fruitList.filter((fruit) => id !== fruit.id)
-        })
+    setCounter((counter) => {
+      return parseInt(counter) + 1;
+    });
 
-        setCash((cash)=>{
-            return parseInt(cash) - parseInt(draggedFruit[0].price)
-        })
+    setCalories((calories) => {
+      return parseInt(calories) + parseInt(draggedFruit[0].cal);
+    });
+  };
 
-        setCounter((counter)=>{
-            return parseInt(counter) + 1
-        })
-        
-        setCalories((calories)=>{
-            return parseInt(calories) + parseInt(draggedFruit[0].cal)
-        })
+  if (cash < 2) {
+    console.log(cash);
+  }
 
-    }
+  const resetGame = () => {
+    setInBasket([]);
 
-    const resetGame = () => {
+    setCash(25);
 
-        setInBasket([])
+    setCounter(0);
 
-        setCash(25)
+    setCalories(0);
 
-        setCounter(0)
+    setFruitsList(fruits);
+  };
 
-        setCalories(0)
+  return (
+    <>
+      <Helmet>
+        <title>Level One</title>
+      </Helmet>
+      <div className="level-one__container">
+        <Header
+          amount={cash}
+          guage={calories >= 32 ? guage100 : calories >= 24 ? guage75 : calories >= 16 ? guage50 : calories >= 8 ? guage25 : guage0}
+        />
+        <div className="level-one__stall">
+          <img src={stall} alt="stall image" />
+        </div>
+        <div className="level-one__basket" ref={drop}>
+          <img src={basket} alt="basket image" className="basket" />
+          <p
+            className="basket__counter"
+            style={{ opacity: counter === 0 ? "0" : "1" }}
+          >
+            {counter}
+          </p>
+          {inBasket?.map(({ fruit, image, id, left, top }) => {
+            return (
+              <BasketFruit
+                id={id}
+                key={id}
+                alt={fruit}
+                image={image}
+                left={left}
+                top={top}
+              />
+            );
+          })}
+        </div>
+        {fruitList?.map(({ fruit, image, cal, price, id }) => {
+          return (
+            <Fruit
+              id={id}
+              key={id}
+              alt={fruit}
+              classs={fruit}
+              image={image}
+              cal={cal}
+              price={price}
+            />
+          );
+        })}
+        <Footer reset={resetGame} />
+      </div>
+    </>
+  );
+};
 
-        setFruitsList(fruits)
-    }
-
-    return (
-        <>
-            <Helmet>
-                <title>Level One</title>
-            </Helmet>
-            <div className="level-one__container">
-                <Header
-                    amount={cash}
-                    guage={calories===0?guage0:guage100}
-                />
-                <div className="level-one__stall">
-                    <img src={stall} alt="stall image"/>
-                </div>
-                <div className="level-one__basket" ref={drop}>
-                    <img src={basket} alt="basket image" className="basket"/>
-                    <p className="basket__counter" style={{opacity: counter === 0 ? '0' : '1'}}>{counter}</p>
-                    {
-                        inBasket?.map(({fruit,image,id,left,top})=>{
-                            return(
-                                <BasketFruit
-                                    id={id}
-                                    key={id}
-                                    alt={fruit}
-                                    image={image}
-                                    left={left}
-                                    top={top}
-                                />
-                            )
-                        })
-                    }
-                </div>
-                {
-                    fruitList?.map(({fruit,image,cal,price,id})=>{
-                        return(
-                            <Fruit
-                                id={id}
-                                key={id}
-                                alt={fruit}
-                                classs={fruit}
-                                image={image}
-                                cal={cal}
-                                price={price}
-                            />
-                        )
-                    })
-                }
-                <Footer
-                    reset={resetGame}
-                />
-            </div>
-            
-        </>
-    )
-}
-
-export default LevelOne
+export default LevelOne;
